8704da21febef04639e93d0257e5fe36987cd5fd
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-09-22 19:39:46 -0300

    fix date fns pattern

--- src/languages/ptBR/common.js
+++ src/languages/ptBR/common.js
@@ -1,8 +1,8 @@
 export default {
   siteTitle: "Meus Investimentos",
   logout: "Logout",
-  dateFormat: "dd/MM/YYYY",
-  dateTimeFormat: "dd/MM/YYYY [-] HH[h]mm",
+  dateFormat: "dd/MM/yyyy",
+  dateTimeFormat: "dd/MM/yyyy [-] HH[h]mm",
   clearLabel: "Limpar",
   back: "Voltar",
   close: "Fechar",
