80bcba6a63c48af17d0cb31dbed9a8e2531d8470
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-10-14 19:02:38 -0300

    Remove test

--- src/common/PrivateLayout/Menu.test.js
+++ src/common/PrivateLayout/Menu.test.js
@@ -1,18 +1,10 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import { shallow } from "enzyme"
-import { findDataTest, expectDataTest } from "app/utils/tests"
+import { findDataTest } from "app/utils/tests"
 import Menu, { menuItems } from "./Menu"
 
 function build() {
   return shallow(<Menu />)
-}
-
-function verifyLinkItem(wrapper, item) {
-  const link = findDataTest(wrapper, "link")
-  expect(link).toHaveLength(1)
-  expect(link).toHaveProp({ button: true, component: Link, to: item.to })
-  expectDataTest(wrapper, "link-text").toHaveProp({ primary: item.text })
 }
 
 describe("Menu", () => {
@@ -22,14 +14,4 @@ describe("Menu", () => {
     const dividers = findDataTest(wrapper, "divider")
     expect(dividers).toHaveLength(dividerItems.length)
   })
-
-  /* it("render link items", () => {
-    const wrapper = build().dive()
-    const linkItems = menuItems.filter(item => !item.divider)
-    const links = findDataTest(wrapper, "link")
-    expect(links).toHaveLength(linkItems.length)
-    linkItems.forEach((item, index) => {
-      verifyLinkItem(links.at(index), item)
-    })
-  }) */
 })
