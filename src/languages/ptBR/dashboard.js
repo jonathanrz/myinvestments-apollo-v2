4e6f60fdc2673d18adc9e6722ffe016a7b66f8b4
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-10-16 17:16:56 -0300

    Table styled

---
+++ src/languages/ptBR/dashboard.js
@@ -0,0 +1,5 @@
+export default {
+  type: "Tipo",
+  value: "Valor",
+  percent: "Percentual"
+}
--- src/pages/dashboard/ByType/index.js
+++ src/pages/dashboard/ByType/index.js
@@ -1,7 +1,42 @@
 import React from "react"
+import { t } from "i18next"
 import { groupBy } from "lodash/fp"
+import { withStyles } from "@material-ui/core/styles"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Paper from "@material-ui/core/Paper"
 import Query from "app/common/Query"
 import query from "./query"
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell)
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 500,
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    width: "100%"
+  },
+  row: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.background.default
+    }
+  }
+})
 
 function groupByData(data) {
   if (!data.investments) {
@@ -16,34 +51,53 @@ function groupByData(data) {
   )
 }
 
-function ByType() {
+function formatForTable(data) {
+  const formatted = Object.keys(data).map((type, index) => ({
+    label: type,
+    value: data[type].reduce((acc, current) => acc + current.value, 0)
+  }))
+  const totalValue = formatted.reduce((acc, current) => acc + current.value, 0)
+  return formatted.map(type => ({ ...type, percent: type.value / totalValue }))
+}
+
+function ByType({ classes }) {
   return (
     <Query query={query}>
       {({ data }) => {
-        const groupedData = groupByData(data)
-        console.log({ groupedData })
+        const formattedData = formatForTable(groupByData(data))
         return (
-          <table>
-            <tr>
-              <th>Tipo</th>
-              <th>Valor</th>
-            </tr>
-            {Object.keys(groupedData).map((type, index) => (
-              <tr key={index}>
-                <td>{type}</td>
-                <td>
-                  {groupedData[type].reduce(
-                    (acc, current) => acc + current.value,
-                    0
-                  )}
-                </td>
-              </tr>
-            ))}
-          </table>
+          <Paper className={classes.root}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <CustomTableCell>{t("dashboard.type")}</CustomTableCell>
+                  <CustomTableCell numeric>
+                    {t("dashboard.value")}
+                  </CustomTableCell>
+                  <CustomTableCell numeric>
+                    {t("dashboard.percent")}
+                  </CustomTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {formattedData.map((type, index) => (
+                  <TableRow key={index} className={classes.row}>
+                    <CustomTableCell component="th" scope="row">
+                      {type.label}
+                    </CustomTableCell>
+                    <CustomTableCell numeric>{type.value}</CustomTableCell>
+                    <CustomTableCell numeric>
+                      {(type.percent * 100).toFixed(2)}%
+                    </CustomTableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
         )
       }}
     </Query>
   )
 }
 
-export default ByType
+export default withStyles(styles)(ByType)
