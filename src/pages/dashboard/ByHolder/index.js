9f6a2cdd4ee405ab839834f9e70706b3858ae677
Jonathan Zanella <jonathanrafaelz@gmail.com>
2018-10-16 17:37:43 -0300

    Extract table

--- src/pages/dashboard/ByHolder/index.js
+++ src/pages/dashboard/ByHolder/index.js
@@ -2,41 +2,9 @@ import React from "react"
 import { t } from "i18next"
 import { groupBy } from "lodash/fp"
 import { withStyles } from "@material-ui/core/styles"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Paper from "@material-ui/core/Paper"
 import Query from "app/common/Query"
 import query from "./query"
-
-const CustomTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
-  },
-  body: {
-    fontSize: 14
-  }
-}))(TableCell)
-
-const styles = theme => ({
-  root: {
-    width: "100%",
-    maxWidth: 500,
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
-  },
-  table: {
-    width: "100%"
-  },
-  row: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.background.default
-    }
-  }
-})
+import Table from "../common/Table"
 
 function groupByData(data) {
   if (!data.investments) {
@@ -57,47 +25,30 @@ function formatForTable(data) {
     value: data[holder].reduce((acc, current) => acc + current.value, 0)
   }))
   const totalValue = formatted.reduce((acc, current) => acc + current.value, 0)
-  return formatted.map(type => ({ ...type, percent: type.value / totalValue }))
+  return formatted.map(type => ({
+    ...type,
+    percent: `${((type.value / totalValue) * 100).toFixed(2)}%`
+  }))
 }
 
-function ByType({ classes }) {
+function ByHolder({ classes }) {
   return (
     <Query query={query}>
       {({ data }) => {
         const formattedData = formatForTable(groupByData(data))
         return (
-          <Paper className={classes.root}>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <CustomTableCell>{t("dashboard.holder")}</CustomTableCell>
-                  <CustomTableCell numeric>
-                    {t("dashboard.value")}
-                  </CustomTableCell>
-                  <CustomTableCell numeric>
-                    {t("dashboard.percent")}
-                  </CustomTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {formattedData.map((holder, index) => (
-                  <TableRow key={index} className={classes.row}>
-                    <CustomTableCell component="th" scope="row">
-                      {holder.label}
-                    </CustomTableCell>
-                    <CustomTableCell numeric>{holder.value}</CustomTableCell>
-                    <CustomTableCell numeric>
-                      {(holder.percent * 100).toFixed(2)}%
-                    </CustomTableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
+          <Table
+            columns={[
+              { header: t("dashboard.holder"), key: "label" },
+              { header: t("dashboard.value"), key: "value", numeric: true },
+              { header: t("dashboard.percent"), key: "percent", numeric: true }
+            ]}
+            content={formattedData}
+          />
         )
       }}
     </Query>
   )
 }
 
-export default withStyles(styles)(ByType)
+export default ByHolder
